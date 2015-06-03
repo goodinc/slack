80ac0219c396ad60348019fdbc8a7715663181ac
deoxxa <deoxxa@fknsrs.biz>
2015-06-03 18:57:16 +1000

    move username into client struct, support emoji icon

--- slack.go
+++ slack.go
@@ -26,26 +26,29 @@ type slackPostMessageRes struct {
 // Client represents a slack api client. A Client is
 // used for making requests to the slack api.
 type Client struct {
-	token string
+	token    string
+	username string
+	icon     string
 }
 
 // NewClient returns a Client with the provided api token.
-func NewClient(token string) *Client {
-	return &Client{token}
+func NewClient(token, username, icon string) *Client {
+	return &Client{token, username, icon}
 }
 
 // SendMessage sends a text message to a specific channel
 // with a specific username.
-func (c *Client) SendMessage(channel, message, username string) error {
-	if channel == "" || message == "" || username == "" {
-		return errors.New("channel, message, and username required")
+func (c *Client) SendMessage(channel, message string) error {
+	if channel == "" || message == "" {
+		return errors.New("channel and message required")
 	}
 
 	payload := url.Values{}
 	payload.Set("token", c.token)
+	payload.Set("username", c.username)
+	payload.Set("icon_emoji", c.icon)
 	payload.Set("channel", channel)
 	payload.Set("text", message)
-	payload.Set("username", username)
 
 	res, err := http.PostForm(fmt.Sprintf("%s/%s", slackAddr, postMessageURI), payload)
 	if err != nil {
